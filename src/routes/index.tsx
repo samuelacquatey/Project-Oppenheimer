d65a4b2195a275966dfa1345bc7be04cdcdb0860
kwasi_dev <samuelacquatey@gmail.com>
2024-06-11 22:42:23 -0700

    Finished setting up qwik, bun, but i was unsuccessful at making the frontend send a message to the console through a button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $} from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+
+
 export default component$(() => {
+  console.log("Hello from the Backend!!");
+
+  const handleClick=$(()=>{
+    console.log("Hello from Frontend!!")
+  });
+
   return (
     <>
       <h1>Hi 👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
-        Happy coding.
+        Happy coding!
       </div>
+      <button onClick$={()=>{handleClick}}>Click me</button>
+      
     </>
   );
+  
 });
 
 export const head: DocumentHead = {
